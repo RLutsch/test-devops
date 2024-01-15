5a026a6a3f1a7cc0f6aaf1705c7ad016b41e6dc1
rlutsch <ross@pulse.tv>
2024-01-15 14:39:53 +0200

    initial commit

---
+++ load.sh
@@ -0,0 +1,9 @@
+cd /home/ubuntu/test-devops
+echo "$(date +"%Y-%m-%d %H:%M:%S") Check if there are changes in the Git repository" >> /var/log/cron.log
+if git fetch origin && ! git diff --quiet main..origin/main; then
+    echo "$(date +"%Y-%m-%d %H:%M:%S") Pulling the latest changes from the Git repository" >> /var/log/cron.log
+    git pull
+else
+    echo "$(date +"%Y-%m-%d %H:%M:%S") No changes in the Git repository" >> /var/log/cron.log
+    exit 0
+fi
